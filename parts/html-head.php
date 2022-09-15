af4d1afdf1f0f4dc5d978352045e8bdc26c10cfb
YI-RU <game665987@gmail.com>
2022-09-15 23:24:15 +0800

    add page

---
+++ parts/html-head.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="zh">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css
+    ">
+    <title>Document</title>
+    <style>
+        .nav-link{
+            font-size: 20px;
+            font-weight: 700;
+        }
+        .car{
+            color: brown;
+        }
+        .logo{
+            font-size: 25px;
+            font-weight: 700;
+            background-color: lightblue;
+            padding: 10px;
+        }
+    </style>
+</head>
+
+<body>
